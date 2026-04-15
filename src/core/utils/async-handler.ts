9fd6813bd92e87d62357dd3d4cd48fc6da5a3c62
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-04-15 15:51:33 +0530

    refactor async-handler to support generic request types for improved type safety

--- src/core/utils/async-handler.ts
+++ src/core/utils/async-handler.ts
@@ -2,15 +2,15 @@
 import type { Request, Response, NextFunction } from 'express';
 
 // type definition for the controller that will be wrapped by the async-handler
-type Controller<T = unknown> = (
-  request: Request,
+type Controller<R extends Request = Request, T = unknown> = (
+  request: R,
   response: Response<T>,
   nextFunction: NextFunction
 ) => unknown | Promise<unknown>;
 
 // function to wrap controllers inside async-handler
-export default function <T = unknown>(controller: Controller<T>) {
-  return function (request: Request, response: Response<T>, nextFunction: NextFunction) {
+export default function <R extends Request = Request, T = unknown>(controller: Controller<R, T>) {
+  return function (request: R, response: Response<T>, nextFunction: NextFunction) {
     return Promise.resolve(controller(request, response, nextFunction)).catch(nextFunction);
   };
 }
