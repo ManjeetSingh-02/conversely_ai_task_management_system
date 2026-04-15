59cf2e52976973fc786ea3d2d3ff7000e9398179
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-04-15 14:28:01 +0530

    refactor generateSignedToken function to use destructured parameters for improved readability

--- src/core/lib/jwt.ts
+++ src/core/lib/jwt.ts
@@ -5,6 +5,14 @@ import jwt from 'jsonwebtoken';
 import type { SignOptions } from 'jsonwebtoken';
 
 // function to generate a signed JWT token
-export function generateSignedToken(payload: object, secretKey: string, options: SignOptions) {
+export function generateSignedToken({
+  payload,
+  secretKey,
+  options,
+}: {
+  payload: object;
+  secretKey: string;
+  options?: SignOptions;
+}) {
   return jwt.sign(payload, secretKey, options);
 }
