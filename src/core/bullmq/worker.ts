8a68e3cac79bae1a273e70c561df63fec37d4a60
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-04-18 21:41:47 +0530

    refactor: update reminder worker to log task due messages

--- src/core/bullmq/worker.ts
+++ src/core/bullmq/worker.ts
@@ -1,13 +1,11 @@
 // internal-imports
-import { logger } from '../logger/winston.js';
 import { redis } from '../database/redis/client.js';
 
 // external-imports
 import { Worker } from 'bullmq';
 
 // create a new worker for sending reminders
-export const worker = new Worker(
-  'reminder',
-  async job => logger.info(`Task: ${job.id} due in 1 hour`),
-  { connection: redis }
-);
+export const worker = new Worker('reminder', async job => console.log(job.data.message), {
+  connection: redis,
+  autorun: true,
+});
